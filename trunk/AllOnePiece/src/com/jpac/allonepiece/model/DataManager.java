cbaf9dffe3ea19a76e76c7aa89dd94fc338dc7a2
carabuena.paulo@gmail.com <carabuena.paulo@gmail.com@855b394f-46ab-2534-e06d-7d30d9995b35>
2013-04-21 10:45:19 +0000

    DataManager.java

---
+++ trunk/AllOnePiece/src/com/jpac/allonepiece/model/DataManager.java
@@ -0,0 +1,11 @@
+package com.jpac.allonepiece.model;
+
+
+public class DataManager {
+
+	private DataManager() { }
+	
+	public static void loadData(DatabaseHandler handler) {
+		int n = handler.getQuestionsCount();
+	}
+}
