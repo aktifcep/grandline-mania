dca56fbc4028d9cd47fcfb39716e408f5cc2060e
carabuena.paulo <carabuena.paulo@gmail.com>
2013-05-25 16:21:06 +0000

    updates

---
+++ AllOnePiece/src/com/jpac/allonepiece/GameFinishedActivity.java
@@ -0,0 +1,10 @@
+package com.jpac.allonepiece;
+
+import android.os.Bundle;
+
+public class GameFinishedActivity extends GameCoreActivity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
